383370bba6ce2903d17c3ea2884b783d5b2bbf1b
Philip Krueck <philip.krueck@apploft.de>
2020-01-13 17:50:50 +0100

    UK-61 removed dependencies

--- Package.swift
+++ Package.swift
@@ -20,7 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "StatefulViewController",
-            dependencies: [],
             path: "StatefulViewController"),
         .testTarget(
             name: "StatefulViewControllerTests",
