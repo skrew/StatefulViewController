d948bb9288e12ba4603923468faec3d0367655e9
eric@skrobs <738170+skrew@users.noreply.github.com>
2020-08-22 19:40:10 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "StatefulViewController",
     platforms: [
-        .iOS(.v10)
+        .macOS(.v10_12),
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v3)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
